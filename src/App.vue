1c453d58263aeb8a5037aaca6c84575fc3863411
oscar-quiroz <kairox.rt@gmail.com>
2021-10-14 12:56:28 -0500

    componentes de autenticacion terminados

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,124 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+    <div class="header">
+      <h1>Banco UN</h1>
+      <nav>
+        <button v-if="is_auth">Inicio</button>
+        <button v-if="is_auth">Cuenta</button>
+        <button v-if="is_auth">Cerrar Sesión</button>
+        <button v-if="!is_auth" v-on:click="loadLogIn">Iniciar Sesión</button>
+        <button v-if="!is_auth" v-on:click="loadSignUp">Registrarse</button>
+      </nav>
+    </div>
+
+    <div class="main-component">
+      <router-view
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+      >
+      </router-view>
+    </div>
+
+    <div class="footer">
+      <h2>Misión TIC 2022</h2>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+      is_auth: false,
+    };
+  },
+  components: {},
+  methods: {
+    verifyAuth: function() {
+      if (this.is_auth == false) this.$router.push({ name: "logIn" });
+    },
+    loadLogIn: function() {
+      this.$router.push({ name: "logIn" });
+    },
+    loadSignUp: function() {
+      this.$router.push({ name: "signUp" });
+    },
+    completedLogIn: function(data) {},
+    completedSignUp: function(data) {},
+  },
+  created: function() {
+    this.verifyAuth();
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0 0 0 0;
+}
+.header {
+  margin: 0%;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #283747;
+  color: #e5e7e9;
+  display: flex;
+  justify-content: space -between;
+  align-items: center;
+}
+.header h1 {
+  width: 20%;
   text-align: center;
-  color: #2c3e50;
 }
+.header nav {
+  height: 100%;
+  width: 20%;
+  display: flex;
+  justify-content: space -around;
 
-#nav {
-  padding: 30px;
+  align-items: center;
+
+  font-size: 20px;
 }
+.header nav button {
+  color: #e5e7e9;
+  background: #283747;
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  border: 1px solid #e5e7e9;
+
+  border-radius: 5px;
+
+  padding: 10px 20px;
 }
+.header nav button:hover {
+  color: #283747;
+  background: #e5e7e9;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  border: 1px solid #e5e7e9;
+}
+.main -component {
+  height: 75vh;
+  margin: 0%;
+  padding: 0%;
+  background: #fdfefe;
+}
+.footer {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 10vh;
+  min-height: 100px;
+  background-color: #283747;
+  color: #e5e7e9;
+}
+.footer h2 {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
